79599f3fc6efeb3608a2a9bcd239c5557033ae76
科学上网 <103273214+KeXueShangWangkexue@users.noreply.github.com>
2022-04-18 06:58:30 +0800

    Update pyexiv2-sample.py

--- kexue/pyexiv2-sample.py
+++ kexue/pyexiv2-sample.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python2.7
-# just a sample for using python-pyexiv2 Library
-# pyexiv2 is a Python binding to exiv2, the C++ library for manipulation of EXIF, IPTC and XMP image metadata. 
 
 import pyexiv2
 import os
